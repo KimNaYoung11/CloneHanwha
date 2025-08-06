ad8cccedf2457f5b65c94c8bb5a20a6ed4954986
nayoung <yeonglin11@gmail.com>
2025-08-06 23:31:08 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.animate').scrolla({
+        mobile: true, //모바일버전시 활성화
+        once:false //스크롤시 딱 한번만 하고싶을땐 true
+    })
+});/* function 함수 어떠한 기능을 호출할 수 있도록 만들어 놓은 표시 메소드 */
+
+ //splitting.js
+$(function(){
+    Splitting();  //대문자로쓴다!!!
+  });
